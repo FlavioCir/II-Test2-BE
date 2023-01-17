9f1af88fa95059cfd305b349918dbe8ae03d297e
Flavio Cirillo <scatto2003@gmail.com>
2023-01-17 16:31:47 +0100

    Esercizi - Giorno 2 - Settimana - 2

---
+++ Esercizio3_G2_S2/src/Esercizio3/Main3.java
@@ -0,0 +1,14 @@
+package Esercizio3;
+
+import java.util.Scanner;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		
+		RubricaTelefonica r1 = new RubricaTelefonica();
+		
+		r1.rubrica();
+	}
+
+}
